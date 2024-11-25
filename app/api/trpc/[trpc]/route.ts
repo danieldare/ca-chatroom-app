e433a32632038b46139d4dd8abd010544f6580c1
Segun Daniel Oluwadare <dodanieloluwadare@gmail.com>
2024-11-25 14:30:45 +0100

    feat(middleware): fix middleware

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,4 +1,5 @@
 import { appRouter } from "@/app/server/trpc";
+import { createContext } from "@/app/server/trpc/context";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 const handler = async (req: Request) => {
